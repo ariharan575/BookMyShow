baee1e5c88955dcec61a05bfb9556b8a789ae7b2
ARIHARAN <fmoon6795@gmail.com>
2026-03-04 13:19:40 +0530

    Final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,59 @@
-import React, {useState } from 'react'
-import { createContext } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.min.js'
-import'bootstrap-icons/font/bootstrap-icons.css'
-import './App.css'
-import {Home} from './LandingPage/Home'
-import { AllMovieList } from './Movie/AllMoviesList'
-import MovieClick from './Movie/MovieClick'
-import { createBrowserRouter, createRoutesFromElements, Navigate, Route, RouterProvider } from 'react-router-dom'
-import { SearchBarMovie } from './Searchbar/SearchBarMovie'
-import NotFound from './componant/NotFound'
+import React, { useState, createContext } from "react";
+import { 
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  Navigate,
+  RouterProvider
+} from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "./App.css";
+
+import { Home } from "./LandingPage/Home";
+import { AllMovieList } from "./Movie/AllMoviesList";
+import MovieClick from "./Movie/MovieClick";
+import { SearchBarMovie } from "./Searchbar/SearchBarMovie";
+import NotFound from "./componant/NotFound";
 
 export const locContext = createContext();
 
 function App() {
+  const [location, setLocation] = useState(
+    localStorage.getItem("city") || ""
+  );
 
-  const [location,setLocation] = useState(localStorage.getItem("city") || "");
-  const citySaved = localStorage.getItem("city")
+  const [popup, setPopup] = useState(
+    localStorage.getItem("city") ? false : true
+  );
 
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route path="/" element={<Navigate to="/explore/home" />} />
+        <Route path="/explore/home" element={<Home />} />
+        <Route path="/explore/home/:city" element={<Home />} />
+        <Route path="/explore/movie/:city" element={<AllMovieList />} />
+        <Route
+          path="/explore/movie/:city/:movies/:id"
+          element={<MovieClick />}
+        />
+        <Route
+          path="/explore/search/:location"
+          element={<SearchBarMovie />}
+        />
+        <Route path="*" element={<NotFound />} />
+      </>
+    )
+  );
 
-  const [popup,setPopup] = useState(citySaved == null || 0 ? true : false )
-
-  const router = createBrowserRouter(createRoutesFromElements(
-  <>
-  <Route path='/' element={<Navigate to='/explore/home'/>}/>
-    <Route path='explore/home' element={<Home/>}/> 
-      <Route path='explore/home/:city' element={<Home/>}/>
-      <Route path='explore/movie/:city' element={<AllMovieList/>}/>
-      <Route path='explore/movie/:city/:movies/:id' element={<MovieClick/>}/>
-      <Route path="/explore/search/:location" element={<SearchBarMovie />} />
-      <Route path='explore/home/:city' element={<Home key={location} />} />
-      <Route path='*' element={<NotFound/>}/>
-  </>
-))
-  
   return (
-    <>
- 
-    <locContext.Provider value={{location,setLocation,popup,setPopup}}>
-  <RouterProvider router={router}>
-   </RouterProvider>
+    <locContext.Provider value={{ location, setLocation, popup, setPopup }}>
+      <RouterProvider router={router} />
     </locContext.Provider>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
